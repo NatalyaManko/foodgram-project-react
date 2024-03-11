721d35ef274df6d879df2ded2756ee692e8a7d49
NataylaManko <you@example.com>
2024-03-11 20:10:32 +0700

    test main

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -10,18 +10,21 @@ from users import models
 class FoodgramAPITestCase(TestCase):
 
     def setUp(self):
-        self.guest_client = APIClient()
+        User = models.User
+        self.user = User.objects.create_user(username='auth_user')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
 
     def test_list_exists(self):
         """Проверка доступности списка задач."""
 
-        response = self.guest_client.get('/api/recipes/')
+        response = self.client.get('/api/recipes/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
         """Проверка создания задачи."""
 
-        data = {'name': 'Test', 'text': 'Test'}
-        response = self.guest_client.post('/api/recipes/', data=data)
+        data = {'user': 'Test', 'recipe': 'Test'}
+        response = self.client.post('/api/recipes/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
-        self.assertTrue(models.Recipe.objects.filter(name='Test').exists())
+        self.assertTrue(models.Favorite.objects.filter(user='Test').exists())
