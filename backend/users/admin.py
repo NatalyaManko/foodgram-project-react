369a91808664c13059bb8aae3d8dd0072bca804c
NataylaManko <you@example.com>
2024-03-11 23:52:31 +0700

    papitka

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.views.decorators.csrf import csrf_exempt
 
 from .models import Follow, User
 
 
-@csrf_exempt
 class UserAdmin(admin.ModelAdmin):
 
     list_display = ('id',
