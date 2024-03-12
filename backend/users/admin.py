b91710867b7571d47d3763893f49c2e3c00470fd
NataylaManko <you@example.com>
2024-03-12 13:03:56 +0700

    ritugj

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -8,13 +8,7 @@ from .models import Follow, User
 class UserAdmin(admin.ModelAdmin):
 
     list_display = '__all__'
-    list_editable = ('username',
-                     'email',
-                     'first_name',
-                     'last_name',
-                     'password',
-                     'is_staff',
-                     'is_active',)
+    list_editable = '__all__'
     search_fields = ('last_name',)
 
 
