abf9de9fceae743859feed8a603b31ba57aad92e
NataylaManko <you@example.com>
2024-03-12 12:35:16 +0700

    group

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -15,7 +15,7 @@ class UserAdmin(admin.ModelAdmin):
                     'password',
                     'is_staff',
                     'is_active',
-                    'groups',
+                    'group',
                     'is_superuser')
     list_editable = ('username',
                      'email',
@@ -24,7 +24,7 @@ class UserAdmin(admin.ModelAdmin):
                      'password',
                      'is_staff',
                      'is_active',
-                     'groups',)
+                     'group',)
     search_fields = ('last_name',)
     list_display_links = None
 
