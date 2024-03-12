d461088f2c88771e3c5400cb90b4a19175fc4564
NataylaManko <you@example.com>
2024-03-12 10:15:35 +0700

    decorator

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
+
 from .models import (Favorite,
                      Ingredient,
                      Recipe,
