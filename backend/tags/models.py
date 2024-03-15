9baefe2701ed611c0776df1700a311bad5874b7a
NataylaManko <you@example.com>
2024-03-15 21:26:38 +0700

    povtor

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -1,5 +1,5 @@
-from colorfield.fields import ColorField
 from django.conf import settings
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -7,10 +7,13 @@ class Tag(models.Model):
     name = models.CharField('Название',
                             max_length=settings.CHAR_FIELD_MAX_LEN,
                             unique=True)
-    color = ColorField('Цветовой код',
-                       max_length=settings.HEX_FIELD_MAX_LEN,
-                       unique=True,
-                       null=False)
+    color = models.CharField('Цвет',
+                             max_length=settings.HEX_FIELD_MAX_LEN,
+                             null=False,
+                             validators=(RegexValidator(
+                                 regex='^#[a-fA-F0-9]{1,6}$',
+                                 message='Цвет должен быть задан кодом '
+                                 + '(например: #E26C2D)'),))
     slug = models.SlugField('Слаг',
                             max_length=settings.CHAR_FIELD_MAX_LEN,
                             unique=True)
