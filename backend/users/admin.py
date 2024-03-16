e66075e27866fa24827a7c1e32a9696f063c596c
NataylaManko <you@example.com>
2024-03-16 13:02:41 +0700

    admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,20 +1,42 @@
-from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 
-UserAdmin.list_filter = ('username', 'email',)
-UserAdmin.list_display += ('id',
-                           'username',
-                           'email',
-                           'first_name',
-                           'last_name',
-                           'password',
-                           'is_staff',
-                           'is_active',
-                           'is_superuser')
-UserAdmin.list_editable += ('username',
-                            'email',
-                            'first_name',
-                            'last_name',
-                            'password',
-                            'is_staff',
-                            'is_active',)
-UserAdmin.list_display_links = ('id',)
+User = get_user_model()
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    def add_to_group(self, request, queryset):
+        group_id = request.POST.get('group_id')
+        group = Group.objects.get(pk=group_id)
+        queryset.update(groups=group)
+
+    add_to_group.short_description = 'Добавить в группу'
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        fieldsets += (('Groups', {'fields': ('groups',)}),)
+
+
+CustomUserAdmin.list_filter = ('username', 'email',)
+CustomUserAdmin.list_display += ('id',
+                                 'username',
+                                 'email',
+                                 'first_name',
+                                 'last_name',
+                                 'password',
+                                 'is_staff',
+                                 'is_active',
+                                 'is_superuser')
+CustomUserAdmin.list_editable += ('username',
+                                  'email',
+                                  'first_name',
+                                  'last_name',
+                                  'password',
+                                  'is_staff',
+                                  'is_active',)
+CustomUserAdmin.list_display_links = ('id',)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
