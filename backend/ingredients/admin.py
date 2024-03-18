feed1b28e0740b7a01b03cf570ec461a9a2c01ae
NataylaManko <you@example.com>
2024-03-18 21:58:25 +0700

    flake8_1

--- backend/ingredients/admin.py
+++ backend/ingredients/admin.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 
 from ingredients.models import Ingredient, Unit
 
+admin.site.register(Unit)
+
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit', )
     search_fields = ('name',)
-
-
-@admin.register(Unit)
-class UnitAdmin(admin.ModelAdmin):
-    list_display = ('fuii_name', 'name',)
