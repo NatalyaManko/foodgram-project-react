b7027422f2ec73fd2ed5cffd1cf82acc8a1cdf5d
NataylaManko <you@example.com>
2024-03-21 11:57:32 +0700

    проверка сайта

--- backend/backend/permissions.py
+++ backend/backend/permissions.py
@@ -10,21 +10,3 @@ class IsAuthorPermission(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or request.user == obj.author
         )
-
-
-class CanViewUserProfile(permissions.BasePermission):
-    """Права на просмотр страниц пользователей"""
-
-    def has_permission(self, request, view):
-
-        if view.action == 'list':
-            return True
-
-        return request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
-
-        if request.method == 'GET':
-            return True
-
-        return request.user.is_authenticated
