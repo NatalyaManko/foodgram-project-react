a46d2634f7c3fe61b5fb4454ccabcea4df21ae94
NataylaManko <you@example.com>
2024-03-21 08:36:48 +0700

    permission_user

--- backend/backend/permissions.py
+++ backend/backend/permissions.py
@@ -9,3 +9,9 @@ class IsAuthorPermission(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or request.user == obj.author
         )
+
+
+class CanViewUserProfile(permissions.BasePermission):
+    """Права на просмотр страниц пользователей"""
+    def has_permission(self, request, view):
+        return True if view.action == 'retrieve' else False
